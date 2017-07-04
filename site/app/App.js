02d4072379bf1672d18eeed59ea925bf94b955ad
Josh Gretz <jgretz@truefit.io>
2017-07-03 20:05:20 -0400

    abstract money and menu, show home text

--- site/app/App.js
+++ site/app/App.js
@@ -1,15 +1,46 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import autobind from 'class-autobind';
 
 import {loadPages} from './features/pages/actions';
+import {Money} from './features/pages/components';
+import {Menu} from './features/menu/components';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+  }
+
   componentWillMount() {
     this.props.loadPages();
   }
 
+  findName() {
+    let {location: {pathname}} = this.props;
+    pathname = pathname.replace('/', '');
+
+    if (!pathname || pathname.length === 0 || pathname === '#') {
+      return 'home';
+    }
+
+    return pathname;
+  }
+
+  renderChildren(name) {
+    return React.Children.map(this.props.children, child => React.cloneElement(child, {name}));
+  }
+
   render() {
-    return this.props.children;
+    const name = this.findName();
+    return (
+      <div>
+        <Money name={name} />
+        <Menu />
+
+        {this.renderChildren(name)}
+      </div>
+    );
   }
 }
 
--- site/app/features/home/components/home.js
+++ site/app/features/home/components/home.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import {Money} from '../../pages/components';
+import {connect} from 'react-redux';
+import {Row, Col} from 'react-bootstrap';
 
-export default () => (
-  <Money name="home" />
-);
+import {pageSelector} from '../../pages/selectors';
+
+const home = ({page}) => {
+  if (!page) {
+    return null;
+  }
+
+  return (
+    <Row>
+      <Col xs={10} xsOffset={1}>
+        <div dangerouslySetInnerHTML={{__html: page.text}} />
+      </Col>
+    </Row>
+  );
+};
+
+const mapStateToProps = (state, props) =>
+({
+  page: pageSelector(state, props),
+});
+
+export default connect(mapStateToProps)(home);
