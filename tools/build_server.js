f3a735064c74e786a26a776d6251ca703f7213d5
Josh Gretz <jgretz@truefit.io>
2017-07-11 15:43:19 -0400

    update build order

--- tools/build_server.js
+++ tools/build_server.js
@@ -12,7 +12,9 @@ rm('lib/*.js');
 exec('babel -d lib/ server/src/index.js');
 exec('babel -d lib/routes server/src/routes');
 exec('babel -d lib/schema server/src/schema');
-exec('babel -d lib/config/index.js server/src/config/index.js');
+exec('babel -d lib/config/ server/src/config/index.js');
 
 mv('lib/server/src/index.js', 'lib/index.js');
+mv('lib/config/server/src/config/index.js', 'lib/config/index.js');
 rm('-rf', 'lib/server');
+rm('-rf', 'lib/config/server');
