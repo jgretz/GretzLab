e67aac4058827e19841319b9f564d12c2541919d
Josh Gretz <jgretz@truefit.io>
2017-07-11 15:47:39 -0400

    add util to the build path

--- tools/build_server.js
+++ tools/build_server.js
@@ -13,6 +13,7 @@ exec('babel -d lib/ server/src/index.js');
 exec('babel -d lib/routes server/src/routes');
 exec('babel -d lib/schema server/src/schema');
 exec('babel -d lib/config/ server/src/config/index.js');
+exec('babel -d lib/util server/src/util');
 
 mv('lib/server/src/index.js', 'lib/index.js');
 mv('lib/config/server/src/config/index.js', 'lib/config/index.js');
