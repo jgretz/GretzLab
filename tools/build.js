270e34948f0e63a0d816162aa358f908d7153d6c
Josh Gretz <jgretz@truefit.io>
2017-07-11 14:53:32 -0400

    update build script to create lib dir

--- tools/build.js
+++ tools/build.js
@@ -3,6 +3,11 @@ require('shelljs/global');
 
 // move over package.json
 echo('Moving package.json ...');
+
+if (find('./lib').length === 0) {
+  mkdir('./lib');
+}
+
 cp('./package.prod.json', './lib/package.json');
 
 // build app
