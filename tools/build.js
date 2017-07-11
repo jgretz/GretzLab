a82f2a117820f42e481bb1c6a3bae30b23833f38
Josh Gretz <jgretz@truefit.io>
2017-07-11 13:58:18 -0400

    include package.json in build to make heroku happy

--- tools/build.js
+++ tools/build.js
@@ -1,3 +1,10 @@
 /* eslint-disable no-var */
+require('shelljs/global');
+
+// build app
 require('./build_server');
 require('./build_site');
+
+// move over package.json
+echo('Finishing Site ...');
+cp('package.json', 'lib/package.json');
