6f9a2754003eeb60fa018da1ee5d1a940e082539
Josh Gretz <jgretz@truefit.io>
2017-07-11 14:44:15 -0400

    add production package.json

--- tools/build.js
+++ tools/build.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-var */
 require('shelljs/global');
 
+// move over package.json
+echo('Moving package.json ...');
+cp('./package.prod.json', './lib/package.json');
+
 // build app
 require('./build_server');
 require('./build_site');
 
-// move over package.json
-echo('Finishing Site ...');
-cp('package.json', 'lib/package.json');
+
