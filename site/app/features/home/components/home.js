b646f261abcc309ae563171a66c4e0cdb7b4ddd7
Josh Gretz <jgretz@truefit.io>
2017-07-13 15:17:22 -0400

    add SEO / meta tag logic

--- site/app/features/home/components/home.js
+++ site/app/features/home/components/home.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Row, Col} from 'react-bootstrap';
+import {Helmet} from 'react-helmet';
 
 import {activePageSelector} from '../../pages/selectors';
 
@@ -13,6 +14,10 @@ const home = ({page}) => {
   return (
     <Row>
       <Col xs={12}>
+        <Helmet>
+          <title>GretzLab</title>
+          <meta name="keywords" content="Gretz,gretz,Josh Gretz,Joshua Gretz,GretzLab" />
+        </Helmet>
         <div dangerouslySetInnerHTML={{__html: page.text}} className="content" />
       </Col>
     </Row>
--- site/app/features/shared/services/makeDataDetail.js
+++ site/app/features/shared/services/makeDataDetail.js
@@ -1,28 +1,71 @@
 /* eslint-disable react/no-danger */
+import _ from 'lodash';
 import React from 'react';
 import {connect} from 'react-redux';
+import {Helmet} from 'react-helmet';
 
 import {activePageSelector} from '../../pages/selectors';
+import replaceAll from './replaceAll';
 
+// helpers
+const title = (page, item) => {
+  let title = `GretzLab - ${_.upperFirst(page.name)}`;
+  if (item) {
+    title = `${title} - ${item.title}`;
+  }
+
+  return title;
+};
+
+const link = (page, item) => {
+  let url = `http://www.gretzlab.com/${page.name}`;
+  if (item) {
+    url = `${url}/${replaceAll(item.title, ' ', '-')}`;
+  }
+
+  return url;
+};
+
+const keywords = (page, item) => {
+  let keywords = 'Gretz,gretz,Josh Gretz,Joshua Gretz,GretzLab';
+  if (item) {
+    keywords = `${item.tags.map(t => t.name).join(',')},${keywords}`;
+  }
+
+  return keywords;
+};
+
+const helmet = (page, item) => (
+  <Helmet>
+    <title>{title(page, item)}</title>
+    <meta name="keywords" content={keywords(page, item)} />
+    <link rel="canonical" href={link(page, item)} />
+  </Helmet>
+);
+
+const defaultDiv = page => (
+  <div dangerouslySetInnerHTML={{__html: page.text}} className="content" />
+);
+
+// detail component
 const detail = ({page, item, makeDetailDiv, makeDefaultDiv}) => {
-  if (!item && !page) {
+  if (!page) {
     return null;
   }
 
-  if (!item && page) {
-    if (makeDefaultDiv) {
-      return makeDefaultDiv(page);
-    }
+  const render = item ? makeDetailDiv(item) : makeDefaultDiv ? makeDefaultDiv(page) : defaultDiv(page); // eslint-disable-line
 
-    return (
-      <div dangerouslySetInnerHTML={{__html: page.text}} className="content" />
-    );
-  }
-
-  return makeDetailDiv(item);
+  return (
+    <div>
+      {helmet(page, item)}
+      <div>
+        {render}
+      </div>
+    </div>
+  );
 };
 
-
+// generator
 export default (activeItemSelector, makeDetailDiv, makeDefaultDiv) => {
   const mapStateToProps = state =>
     ({
