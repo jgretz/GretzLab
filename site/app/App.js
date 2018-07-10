eb4d584f6c44cde6ee770d6e5eb7b80bf2a92fda
Josh Gretz <jgretz@truefit.io>
2018-07-10 15:19:00 -0400

    add header

--- site/app/App.js
+++ site/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {withRouter} from 'react-router';
 
+import {Header} from './features/header/components';
 import {Routes} from './features/shared/components';
 
 const App = () => (
   <div className="wrapper">
+    <Header />
     <Routes />
   </div>
 );
